6d31e756004f92a603298125ead29e7761e4a6bc
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2020-12-20 11:42:13 +0800

    Add function to get a specific language from `Text`

--- src/types/Text.ts
+++ src/types/Text.ts
@@ -1,3 +1,13 @@
 export interface Text {
-  [languageCode: string]: string;
+  [language: string]: string;
+}
+
+export function getText(text: Text, language: string): string {
+  let result = text[language];
+  if (!result) {
+    const [first] = Object.keys(text);
+    result = text[first];
+    console.warn(`The "${language}" language is not available for the "${result}" question; defaulting to the "${first}" language`);
+  }
+  return result;
 }
