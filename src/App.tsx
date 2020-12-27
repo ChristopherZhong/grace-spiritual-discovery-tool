90282d9b9515d51151aabbf2e71bb04c2cd2cc6f
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2020-12-27 09:16:37 +0800

    Remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Questions } from './components/Questions';
 import { CssBaseline } from '@material-ui/core';
