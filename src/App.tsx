7f2f4d2923890ba1514a71c7b8da0a2129130840
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2021-01-03 11:13:03 +0800

    Move `CssBaseline` outside of `App`

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 import { Questions } from './components/Questions';
-import { CssBaseline } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
-      <CssBaseline/>
       <header className="App-header">
         Grace Spiritual Discovery Tool
       </header>
