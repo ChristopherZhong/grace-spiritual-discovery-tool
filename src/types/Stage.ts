1e9bf06f6959d68e7c9218b8f97abaf656fbd8d2
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2021-01-16 22:32:29 +0800

    Add a range to stage

--- src/types/Stage.ts
+++ src/types/Stage.ts
@@ -1,7 +1,17 @@
 import { MultilingualText } from './MultilingualText';
+import stagesJson from '../assets/stages.json';
 
 export interface Stage {
   description: MultilingualText
   id: MultilingualText
   name: MultilingualText
+  range: {
+    min: number
+    max: number
+  }
+}
+
+export function loadStages(): ReadonlyArray<Stage> {
+  console.log(`Loading ${stagesJson.length} stages ...`);
+  return stagesJson;
 }
