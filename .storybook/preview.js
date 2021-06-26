7c11373dd6d6c249b3e07acdfb99c4f6abdfe3d0
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2021-06-26 11:51:16 +0800

    Add language to the storybook toolbar

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,39 @@
+import { LanguageContext } from '../src/contexts/language';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+    actions: { argTypesRegex: '^on[A-Z].*' },
+};
+
+export const globalTypes = {
+    language: {
+        name: 'Language',
+        description: 'Global language context for components',
+        defaultValue: 'en',
+        toolbar: {
+            icon: 'globe',
+            // Array of plain string values or MenuItem shape (see below)
+            items: [
+                { value: 'en', title: 'English' },
+                { value: 'zh', title: '中文' },
+                { value: 'es', title: 'Español' },
+            ],
+            // Property that specifies if the name of the item will be displayed
+            showName: true,
+        },
+    },
+};
+
+const withLanguageProvider = (Story, context) => {
+    const value = {
+        language: {
+            code: context.globals.language,
+        },
+    };
+    return (
+        <LanguageContext.Provider value={value}>
+            <Story {...context} />
+        </LanguageContext.Provider>
+    );
+};
+
+export const decorators = [withLanguageProvider];
